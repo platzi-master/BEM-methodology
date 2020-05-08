14dae8c0ffe47d37bdc9f07c67fd0bb47ef9ba56
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-05-07 23:05:32 -0500

    'base'

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/components/App.styl';
+import Header from './Header';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default App;
